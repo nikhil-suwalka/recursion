0ad8e2fa99185d5a10e6f99a2fdc44454d7bdc87
ShristiBisht <shristibisht2305@gmail.com>
2020-03-13 21:13:41 +0530

    Made separate files for background image(bg.php) and QR scanner(QR.php) and included in index.php based on the device.
    i1.php and index.php is same and index.php is used everywhere.

---
+++ website-comp/QR.php
@@ -0,0 +1,43 @@
+<video id="preview" class="p-1 border" style="position: fixed;right: 0;bottom: 0;min-width: 100%;min-height: 100%;"></video>
+<section id="scanner" class="content-section text-center">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+                <div class="col-sm-12">
+
+                </div>
+                <script type="text/javascript">
+                    var scanner = new Instascan.Scanner({
+                        video: document.getElementById('preview'),
+                        scanPeriod: 5,
+                        mirror: false
+                    });
+                    scanner.addListener('scan', function (content) {
+                        alert(content);
+                        //window.location.href=content;
+                    });
+                    Instascan.Camera.getCameras().then(function (cameras) {
+                        if (cameras.length > 0) {
+                            scanner.start(cameras[0]);
+                            $('[name="options"]').on('change', function () {
+                                if (cameras[0] != "") {
+                                    scanner.start(cameras[0]);
+                                } else {
+                                    alert('No Front camera found!');
+                                }
+                            });
+                        } else {
+                            console.error('No cameras found.');
+                            alert('No cameras found.');
+                        }
+                    }).catch(function (e) {
+                        console.error(e);
+                        alert(e);
+                    });
+                </script>
+            </div>
+
+        </div>
+    </div>
+</section>
---
+++ website-comp/bg.php
@@ -0,0 +1,9 @@
+<style>
+    body {
+        background-image: url('assets/img/demo.jpg');
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        /*background-size: cover;*/
+        background-size: 100% 100%;
+    }
+</style>
